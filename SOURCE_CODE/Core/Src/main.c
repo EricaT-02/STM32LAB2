605e8d7b83f9c453b2849ea0b8d37c0007b913ad
phuongtrinhh_HCMUT <phuongtrinhh0212@gmail.com>
2025-09-16 21:24:55 +0700

    Init for Ex6

--- SOURCE_CODE/Core/Src/main.c
+++ SOURCE_CODE/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "software_timer.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -55,7 +55,21 @@ static void MX_TIM2_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int timer0_counter = 0;
+int timer0_flag = 0;
+int TIMER_CYCLE = 10;
 
+void setTimer0(int duration) {
+	timer0_counter = duration/TIMER_CYCLE;
+	timer_flag = 0;
+}
+
+void timerRun() {
+	if (timer0_counter > 0) {
+		timer0_counter--;
+		if (timer0_counter <= 0) timer0_flag = 1;
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -92,8 +106,14 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  setTimer0(1000);
+  int hour = 15 , minute = 8 , second = 50;
   while (1)
   {
+	  if (timer0_flag == 1) {
+		  HAL_GPIO_TogglePin(GPIOA, LED_RED_Pin);
+		  setTimer0(2000);
+	  }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
